a9bb104aba935f661a9e0b6ae8f8ea91caa1e6db
dusndjnks <mhdaffan0000@gmail.com>
2024-04-24 13:55:11 +0530

    first commit

---
+++ try.js
@@ -0,0 +1,44 @@
+// let arr=[1,2,3,4,5]
+// console.log(arr.fill("z",1,3));
+
+
+// let  d=[10,20,30,40,50]
+// console.log(d.fill("100",2,4));
+
+
+// let greeting=()=>{
+//     console.log("hey good morning");
+// }
+// setTimeout(greeting,5000)
+
+
+// let iseven=(num)=>{
+//  return num%2===0
+// }
+// let arr=[1,2,3,4,5]
+// let output =arr.filter(()=>{})
+// console.log(output);
+
+// let arr=["html","css","javascript","nodejs","readjs"]
+// let output=arr.filter((item)=>{
+//     return item.length<4
+// })
+// console.log(output);
+
+
+// let arr=[1,2,3,4,5]
+// let output=arr.every((num)=>{
+//     return num %2!==0
+// })
+// console.log(output);
+
+// let arr=[1,3,5]
+// let output=arr.some((num)=>{
+//     return num %2==0
+// })
+//  console.log(output);
+
+let arr=[1,2,3,4,5]
+let output=arr.forEach((item)=>{
+    console.log(item*2);
+})
